ed954c931f3036826f87164ba21cb169e812748b
Ashenaspirations <ashenaspirations@gmail.com>
2025-06-29 21:49:03 -0400

    Replace hard coded book paths with sys.argv arguments

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import sys
 from stats import get_num_words
 from stats import character_tracker
 from stats import sort_on
 
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
 
 def main():
-    words = get_num_words("./books/frankenstein.txt")
+    book_path = sys.argv[1]
+    words = get_num_words(book_path)
     print(f"Found {words} total words")
-    character_dict = character_tracker("./books/frankenstein.txt")
+    character_dict = character_tracker(book_path)
     sorted_dict = sort_on(character_dict)
     # print(character_dict)
     print(sorted_dict)
